3e5eec3a257774e64f96e99f03ee83ca4429df60
Soukie007 <Nmsoukmandjiev007@outlook.com>
2026-05-27 10:02:14 +0200

    debugging tools added

--- source/main.c
+++ source/main.c
@@ -11,6 +11,8 @@
 #define SPEED 2         // Scroll speed
 #define MAP_X 32       // Map width (animated blocks are 2x2, 128 / 2 = 64)
 #define MAP_Y 32        // Map height (animated blocks are 2x2, 64 / 2 = 32)
+#define GRID_X 64
+#define GRID_Y 64
 #define ROW_LENGTH 16
 #define TOTAL_BLOCKS 4
 #define TILE_LAYER 3
@@ -67,7 +69,7 @@ void initBlocks(){
 }
 
 int WATER_SPEED;
-int TILE_MAP[MAP_Y][MAP_X]; // 512 pixels, 32 tiles, every frame is 2x2 (16/8) tiles
+int TILE_MAP[GRID_Y][GRID_X]; // 512 pixels, 32 tiles, every frame is 2x2 (16/8) tiles
 
 inline int coordsToTile(int coord){
     return coord / 16;
@@ -109,10 +111,16 @@ void initialise(){
     NF_Set2D(0, 0);
     NF_Set2D(1, 0);
 
+
     NF_InitTiledBgBuffers();
     NF_InitTiledBgSys(0);       
     NF_InitTiledBgSys(1);   
+
+    NF_InitTextSys(0);
+    NF_LoadTextFont("fnt/default", "normal", 256, 256, 0);
+    NF_CreateTextLayer(0, 0, 0, "normal");
 }
+
 
 int main(int argc, char **argv)
 {
@@ -123,7 +131,7 @@ int main(int argc, char **argv)
     
     NF_LoadTilesForBg("bg/tiles", "tiles", 512, 512, 0, TOTAL_BLOCKS);
 
-    NF_CreateTiledBg(0, TILE_LAYER, "tiles");
+    //NF_CreateTiledBg(0, TILE_LAYER, "tiles");
     NF_CreateTiledBg(1, TILE_LAYER, "tiles");
     //InitAnimatedBg();
     touchPosition touch_pos;
@@ -131,8 +139,10 @@ int main(int argc, char **argv)
 
     int touchTileX = 0;
     int touchTileY = 0;
-    int scrollX = 0;
-    int scrollY = 0;
+    int scrollX = 128;
+    int scrollY = 160;
+    int minimumX = 0;
+    
 
     while (1)
     {
@@ -156,16 +166,27 @@ int main(int argc, char **argv)
                 if (buttonsHeld & KEY_RIGHT) { scrollX += SPEED; if (scrollX > 255)  scrollX = 255; } // 
                 if (buttonsHeld & KEY_UP)    { scrollY -= SPEED; if (scrollY < 0)    scrollY = 0; }
                 if (buttonsHeld & KEY_DOWN)  { scrollY += SPEED; if (scrollY > 319)   scrollY = 319; } // 
+                char buffer[64];
+                NF_ClearTextLayer(0, 0); 
+                snprintf(buffer,sizeof(buffer),"X px %d Y px %d",scrollX,scrollY);
+                NF_WriteText(0,0,1,1,buffer);
 
-                NF_ScrollBg(1, 3, scrollX, scrollY);
-                swiWaitForVBlank();
+                if(scrollX < 16 || scrollX > 240 /*512-16-256*/ || scrollY < 16 || scrollY > 304 /*512 - 192  - 16*/){
+                    NF_WriteText(0,0,1,2,"T");
+                } else {
+                    NF_WriteText(0,0,1,2,"N");
+                }
+                break;
+
 
         }
          
 
         
 
-      
+        NF_ScrollBg(1, 3, scrollX, scrollY);
+        swiWaitForVBlank();
+        NF_UpdateTextLayers();  
     }
     return 0;
     
