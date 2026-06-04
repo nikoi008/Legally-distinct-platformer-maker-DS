6d71f48a326c4a3d0fd718ae6dbc2877f536fe4b
Soukie007 <Nmsoukmandjiev007@outlook.com>
2026-06-04 14:12:04 +0100

    Automated camera variables, fixed jittering, removed unnecessary code

--- source/main.c
+++ source/main.c
@@ -11,8 +11,8 @@
 #define SPEED 2         // Scroll speed
 #define MAP_X 32       // Map width (animated blocks are 2x2, 128 / 2 = 64)
 #define MAP_Y 32        // Map height (animated blocks are 2x2, 64 / 2 = 32)
-#define GRID_X 128
-#define GRID_Y 128
+#define GRID_X 1024
+#define GRID_Y 1024 //16kx16k may be a bit overkill
 #define ROW_LENGTH 16
 #define TOTAL_BLOCKS 5
 #define TILE_LAYER 3
@@ -24,8 +24,6 @@ typedef enum{
 gameStates state = EDITOR;
 
 typedef struct{
-    //char* name;
-    //int blockID; necessary?
     bool solid;
     void (*ifTouched)(void);
     int topLeftTile;
@@ -33,8 +31,6 @@ typedef struct{
     int bottomLeftTile;
     int bottomRightTile;
 }block;
-int ogX = 0;
-int ogY = 0;
 block blocks[TOTAL_BLOCKS] = {};
 
 
@@ -76,8 +72,8 @@ void initBlocks(){
 
 }
 
-int WATER_SPEED;
-int TILE_MAP[GRID_Y][GRID_X]; // 512 pixels, 32 tiles, every frame is 2x2 (16/8) tiles
+
+u8 TILE_MAP[GRID_Y][GRID_X]; // 512 pixels, 32 tiles, every frame is 2x2 (16/8) tiles
 
 inline int coordsToTile(int coord){
     return coord / 16;  
@@ -91,17 +87,18 @@ void addTile(int tileX, int tileY){
     else
         TILE_MAP[tileY + 10][tileX + 8]++;
 }
-void UpdateTiles(int *originTileX, int *originTileY)
+
+void updateTiles(int originTileX, int originTileY)
 {
-    for(int y = *originTileY; y < *originTileY + MAP_Y; y++){
-        for(int x = *originTileX; x < *originTileX + MAP_X; x++){
-            int coordX = x - *originTileX;
-            int coordY = y - *originTileY;
+    for(int y = originTileY; y < originTileY + MAP_Y; y++){
+        for(int x = originTileX; x < originTileX + MAP_X; x++){
+            int coordX = x - originTileX;
+            int coordY = y - originTileY;
             int blockID = 0;
             if(x >= 0 && x < GRID_X && y >= 0 && y < GRID_Y)
                 blockID = TILE_MAP[y][x];
             else
-                blockID = 4; // out of bounds debug block
+                blockID = 4; 
             NF_SetTileOfMap(1, TILE_LAYER, coordX*2,   coordY*2,   blocks[blockID].topLeftTile);
             NF_SetTileOfMap(1, TILE_LAYER, coordX*2+1, coordY*2,   blocks[blockID].topRightTile);
             NF_SetTileOfMap(1, TILE_LAYER, coordX*2,   coordY*2+1, blocks[blockID].bottomLeftTile);
@@ -135,22 +132,18 @@ void input(u16 buttonsDown, u16 buttonsHeld,int *cameraX, int *cameraY,int *scro
     if (buttonsHeld & KEY_LEFT){ 
         *scrollX -= SPEED; 
         *cameraX -= SPEED; 
-        //if (*scrollX < 0)  *scrollX = 0; 
     }
     if (buttonsHeld & KEY_RIGHT){ 
         *scrollX += SPEED; 
         *cameraX += SPEED; 
-        //if (*scrollX > 255)  *scrollX = 255; 
     }  
     if (buttonsHeld & KEY_UP){ 
         *scrollY -= SPEED; 
         *cameraY -= SPEED; 
-        //if (*scrollY < 0) *scrollY = 0; 
     }
     if (buttonsHeld & KEY_DOWN){
         *scrollY += SPEED;
         *cameraY += SPEED;  
-        //if (*scrollY > 319)  *scrollY = 319; 
     }
     
     if (buttonsDown & KEY_TOUCH){
@@ -159,10 +152,8 @@ void input(u16 buttonsDown, u16 buttonsHeld,int *cameraX, int *cameraY,int *scro
         *touchTileX = coordsToTile(touch_pos->px + *cameraX );
         *touchTileY = coordsToTile(touch_pos->py + *cameraY );
 
-        //UpdateTiles(&originTileX,&originTileY);
-        
         addTile(*touchTileX,*touchTileY); 
-        UpdateTiles(originTileX, originTileY); 
+        updateTiles(*originTileX, *originTileY); 
     }
     int valX = touch_pos->px + *cameraX ;
     int valY = touch_pos->py + *cameraY;
@@ -183,27 +174,21 @@ int main(int argc, char **argv)
 
     
     NF_LoadTilesForBg("bg/tiles", "tiles", 512, 512, 0, TOTAL_BLOCKS);
-
-    //NF_CreateTiledBg(0, TILE_LAYER, "tiles");
     NF_CreateTiledBg(1, TILE_LAYER, "tiles");
-    //InitAnimatedBg();
     touchPosition touch_pos;
     initBlocks();
 
     int touchTileX = 0;
     int touchTileY = 0;
-    int scrollX = 128;//should scroll values be centered?  desync addtile
+    int scrollX = 128;//should scroll values be 0? they desync addtile
     int scrollY = 160;
-    int cameraX = 384; //center of canvas
-    int cameraY = 416;
+    int cameraX = ((GRID_X* 16)-256) / 2;   
+    int cameraY = ((GRID_Y * 16) - 192) / 2;     
     int cameraTileX = cameraX / 16;
     int cameraTileY = cameraY / 16;
 
-    int originTileX = cameraTileX; //decides the origin of where to start drawing
+    int originTileX = cameraTileX;
     int originTileY = cameraTileY;
-
-
-    
 
     while (1)
     {
@@ -232,24 +217,23 @@ int main(int argc, char **argv)
                 char ocord[64];
                 snprintf(ocord,sizeof(ocord),"oX %d oY %d",originTileX,originTileY);
                 NF_WriteText(0,0,1,5,ocord);
-
-                if(scrollX < 16 || scrollX > 240 /*512-16-256*/ || scrollY < 16 || scrollY > 304 /*512 - 192  - 16*/){
+                int jitteroffsetX = cameraX % 16;
+                int jitterOffsetY = cameraY % 16;
+                cameraTileX = cameraX / 16;
+                cameraTileY = cameraY / 16;
+                if(scrollX < 16 || scrollX > 240 /*512-16-256*/ || scrollY < 16 || scrollY > 304 /*512 - 192 - 16*/){
                     NF_WriteText(0,0,1,2,"T");
-                    scrollX = 128;
-                    scrollY = 160;
+                    scrollX = 128 + jitteroffsetX ;
+                    scrollY = 160 + jitterOffsetY;
                     updateOrigin(&originTileX,&originTileY,&cameraTileX,&cameraTileY);
-                    UpdateTiles(&originTileX, &originTileY); 
+                    updateTiles(originTileX, originTileY); 
 
                 } else {
                     NF_WriteText(0,0,1,2,"N");
                 }
-                //UpdateTiles(&originTileX,&originTileY);
-                cameraTileX = cameraX / 16;
-                cameraTileY = cameraY / 16;
-               
+
                 
-                ogX = originTileX;
-                ogY = originTileY;
+
                 break;
 
 
@@ -259,7 +243,9 @@ int main(int argc, char **argv)
         
 
         NF_ScrollBg(1, 3, scrollX, scrollY);
-        swiWaitForVBlank();
+     
+            swiWaitForVBlank();
+
         NF_UpdateTextLayers();  
     }
     return 0;
